aa5c7e9e4a4995d9dcc3ea48dfe5fcded90308b6
manic-cs <rinard.guy@protonmail.com>
2026-04-16 18:01:06 -0400

    Enhance User model with Todo relationship
    Added relationship between User and Todo models.

--- app/models/user.py
+++ app/models/user.py
@@ -1,13 +1,22 @@
-from sqlmodel import Field, SQLModel
-from typing import Optional
+from sqlmodel import Field, SQLModel, Relationship
+from typing import Optional, List
 from pydantic import EmailStr
 
-
-class UserBase(SQLModel,):
+class UserBase(SQLModel):
     username: str = Field(index=True, unique=True)
     email: EmailStr = Field(index=True, unique=True)
     password: str
-    role:str = ""
+    role: str = ""
 
 class User(UserBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
+
+    todos: List["Todo"] = Relationship(back_populates="user")
+
+class Todo(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key="user.id")
+    text: str
+    done: bool = Field(default=False)
+
+    user: User = Relationship(back_populates="todos")
